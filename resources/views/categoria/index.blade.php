578bfc15ba570ce9583e78c5a18e2f5cb70a4c15
MolanoSantiago <molanosantiagoplay@gmail.com>
2022-06-22 00:06:02 -0500

    Mi primer commit

---
+++ resources/views/categoria/index.blade.php
@@ -0,0 +1,71 @@
+@extends('principal')
+
+@section('contenido')
+<div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Categorías</h3>
+         <br>
+          <a href="{{ url('/categoria/crear') }}" title="Crear"  class="btn btn-primary">
+              Crear
+          </a>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body p-0">
+          <table class="table table-striped projects">
+              <thead>
+                  <tr>
+                      <th style="width: 2%">
+                          Id
+                      </th>
+                      <th style="width: 20%">
+                          Nombre
+                      </th>
+                      <th style="width: 30%">
+                          Descripción
+                      </th>
+                      <th style="width: 30%">Acciones
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                @foreach($categorias as $categoria)
+                  <tr>
+                      <td>
+                          {{ $categoria->id }}
+                      </td>
+                      <td>
+                          {{ $categoria->nombre }}
+                      </td>
+                      <td>
+                          {{ $categoria->descripcion }}
+                      </td>
+
+                      <td class="project-actions text-right">
+                        <form action="{{ url('/categorias/eliminar', $categoria) }}" method="POST">
+                          <a class="btn btn-info btn-sm" href="{{ url('/categoria/editar', $categoria->id) }}">
+                              <i class="fas fa-pencil-alt">
+                              </i>
+                              Editar
+                          </a>
+                          @csrf
+                          @method('DELETE')
+                          <button type="submit" class="btn btn-danger btn-sm">
+                            <i class="fas fa-trash"></i>Eliminar</button>
+                        </form>
+                      </td>
+                  </tr>
+                @endforeach
+              </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+@endsection
---
+++ resources/views/categoria/registrar.blade.php
@@ -0,0 +1,27 @@
+@extends('principal')
+
+@section('contenido')
+<div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Categorías</h3>
+         <br>
+          <a href="{{ url('/categoria/crear') }}" title="Crear"  class="btn btn-primary">
+              Crear
+          </a>
+        </div>
+        <div class="row">
+            <form name="frmRegistrar" method="POST" action="{{ url('/categoria/guardar') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" />
+                </div>
+                <div class="form-group">
+                    <label for="descripcion">Descripción:</label>
+                    <input type="text" id="descripcion" name="descripcion" />
+                </div>
+                <button type="submit" class="btn btn-primary">Registrar</button>
+            </form>
+        </div>
+</div>
+@endsection
